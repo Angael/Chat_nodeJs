5ed9e3592bb89bd2f4e8bbf7af927f346b35ed3f
Angael <krzysztofwidacki@gmail.com>
2017-09-01 12:26:18 +0200

    cutDown8080 port websocket chat

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var clientMap = new Array();
 
 console.log("starting...");
 var wss = new WebSocketServer({
-  port:8001
+  port:8080
 });
 
 // LIB escape message HTML
@@ -161,44 +161,7 @@ wss.on("connection", (ws, req) => {
     // }
   }, 2000);
 })();
-
-
-//Add html site express serving
-//=============================================================================
-var express    = require('express');        // call express
-var fs = require('fs');
-var app        = express();
-var pug = require('pug');
-
-var port = process.env.PORT || 8080;        // set our port
-
-
-// ROUTES FOR OUR API
-// =============================================================================
-app.get("/", function(req, res){
-    console.log("home directory");
-    fs.readFile('./public/index.pug', function (err, pugHtml) {
-        if (err) {
-            throw err;
-        }
-        //get function from index.pug
-        var fn = pug.compile(pugHtml.toString());
-        //change title message. YouAreUsingPug changes one if statement
-        var html = fn();
-
-        res.writeHeader(200, {"Content-Type": "text/html"});
-        res.write(html);
-        res.end();
-    });
-});
-
-
-app.set('views', __dirname + '/public');
-app.set('view engine', 'pug')
-app.use(express.static('public')); // Allows for files in public to be accessible like so: "localhost:4000/login.html"
-
 // START THE SERVER
 // =============================================================================
-app.listen(port);
-console.log('Chat hosted on port ' + port);
-console.log('Websocket port: ' + 8001);
+
+console.log('Websocket port: ' + 8080);
